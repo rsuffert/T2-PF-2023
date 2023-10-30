58a3efecff9a88b62be1e48c588d02224139fd02
Ricardo Süffert <ricardobsuffert@gmail.com>
2023-10-30 01:00:15 -0300

    add loop to the end of the game (user may continue playing)

--- app/hangman.hs
+++ app/hangman.hs
@@ -1,5 +1,6 @@
 import System.IO
 import System.Random
+import Data.Char
 
 main :: IO ()
 main = forca
@@ -33,11 +34,25 @@ play palavra revealedWord = do
         let guessWord = revealedWord ++ [guessLine !! 0] -- append the guessed character to the list of correct guesses
         let result = match guessWord           -- get the word with the characters that have been guessed correctly
         putStrLn ("\tResultado: " ++ result)
-        if result == palavra then putStrLn "Você acertou! Parabéns, você venceu!"
-        else play palavra guessWord
+        if result == palavra then do
+            putStrLn "Você acertou! Parabéns, você venceu!"
+            retry
+        else 
+            play palavra guessWord
     else do
         putStrLn "\tVocê só pode chutar um caractere por vez. Por favor, tente novamente."
         play palavra revealedWord
     where 
         match :: String -> String
         match chute = [if elem x chute then x else '-' | x <- palavra]
+        retry :: IO ()
+        retry = do
+            putStr "Você quer jogar de novo? [s/n] "
+            hFlush stdout -- flush the output buffer to print prompt before witing for user input
+            response <- getLine
+            let responseU = map toUpper response
+            if responseU == "S" then forca
+            else if responseU == "N" then putStrLn "\tObrigado por jogar!"
+            else do
+                putStrLn "\tOpção inválida. Por favor, tente novamente."
+                retry
