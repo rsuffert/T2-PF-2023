1ea00e8d594e2adb19d36ea71539b882e729f4e8
Ricardo Süffert <ricardobsuffert@gmail.com>
2023-11-05 15:46:55 -0300

    improve list of guesses and condition for the end of the game

--- app/hangman.hs
+++ app/hangman.hs
@@ -10,13 +10,13 @@ forca :: IO ()
 forca = do
     putStrLn "\n----------------------- JOGO DA FORCA -----------------------"
     putStrLn "INSTRUÇÕES:"
-    putStrLn "  - Exceto pelos comandos especiais abaico, você só pode chutar uma letra por vez."
+    putStrLn "  - Exceto pelos comandos especiais abaixo, você só pode chutar uma letra por vez."
     putStrLn "  - Se quiser uma dica, digite ':hint' como seu chute."
     putStrLn "  - Se quiser desistir e revelar a palavra, digite ':quit' como seu chute.\n"
     (mysteryWord, hint) <- pickRandomWord "words.txt"
     let wordHidden = ['-' | _ <- mysteryWord]
     putStrLn ("Jogador, a dica inicial da palavra que você deve adivinhar é: " ++ wordHidden)
-    play mysteryWord wordHidden hint
+    play mysteryWord wordHidden hint ""
 
 pickRandomWord :: String -> IO (String, String)
 pickRandomWord fileName = do
@@ -32,8 +32,8 @@ pickRandomWord fileName = do
             let wordList = words contents
             return wordList
 
-play :: String -> String -> String -> IO ()
-play palavra revealedWord hint = do
+play :: String -> String -> String -> String -> IO ()
+play palavra revealedWord hint missedChars = do
     (palavra, revealedWord, hint) <- checkWordChange palavra revealedWord hint -- evaluate if we're changing the word
     putStr "SEU CHUTE: "
     hFlush stdout -- flush the output buffer to print prompt before witing for user input
@@ -43,19 +43,23 @@ play palavra revealedWord hint = do
         return ()
     else if (map toUpper guessLine) == ":HINT" then do
         putStrLn ("\tSua dica é: '" ++ hint ++ "'.")
-        play palavra revealedWord hint
+        play palavra revealedWord hint missedChars
     else if length guessLine == 1 then do
         let guessWord = revealedWord ++ [guessLine !! 0] -- append the guessed character to the list of correct guesses
         let result = match palavra guessWord           -- get the word with the characters that have been guessed correctly
+        let newMissedChars = checkError revealedWord result missedChars (guessLine !! 0)
+        putStrLn ("\tChutes incorretos: " ++ newMissedChars)
         putStrLn ("\tResultado: " ++ result)
         if result == palavra then do
             putStrLn "Você acertou! Parabéns, você venceu!"
             retry
+        else if length newMissedChars `div` 2 == 6 then -- the hangman has been fully drawn and the user has lost the game
+            putStrLn "Suas vidas acabaram. Você perdeu. :("
         else 
-            play palavra result hint
+            play palavra result hint newMissedChars
     else do
         putStrLn "\tVocê só pode chutar uma letra por vez. Por favor, tente novamente."
-        play palavra revealedWord hint
+        play palavra revealedWord hint missedChars
     where 
         match :: String -> String -> String
         match palavra chute = [if elem x chute then x else '-' | x <- palavra]
@@ -82,3 +86,7 @@ play palavra revealedWord hint = do
                     return (newMysteryWord, newRevealedMysteryWord, newHint)
                 else return (mysteryWord, revealedMysteryWord, hint)
             else return (mysteryWord, revealedMysteryWord, hint)
+        checkError :: String -> String -> String -> Char -> String
+        checkError prevRevealedWord newRevealedWord missedChars guessedChar 
+            | newRevealedWord == prevRevealedWord && not (elem guessedChar missedChars) = missedChars++[guessedChar]++" " -- the user's guess was incorrect and they have not been penalized for it yet
+            | otherwise = missedChars
